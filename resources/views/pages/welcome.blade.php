83f9f163008d4602a62c436f49bafbbe07571392
Chloe Lerit <chloewebcraft@gmail.com>
2020-02-15 19:55:06 -0800

    minor updates

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,4 +3,5 @@
         <p align="center"><b>Bass Padawan</b><br />
         Bass Player of local San Diego band <a href="https://www.transientpenguins.com" target="_blank">Transient Penguins</a></p>
         <img src="{{URL('/images/chloebasscraft.png')}}" alt="ChloeBassCraft" width="400" style="display: block;margin-left: auto;margin-right: auto;" />
+        <br />
 @stop
